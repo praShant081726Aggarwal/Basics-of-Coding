c54e83e69604d912d98a2fa716545395c999ee0f
unknown <aggarwalprashant235@gmail.com>
2024-01-25 00:26:24 +0530

    Sum of first N natural numbers

--- _01Sum_N.java
+++ _01Sum_N.java
@@ -1,6 +1,17 @@
+// Sum of first N natural numbers
+import java.util.Scanner;
+
 public class _01Sum_N {
 
     public static void main(String[] args) {
         System.out.println("Hello Praashant");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number : "); // to prompt user to enter a number
+        int n = sc.nextInt(); // to take input 
+        sc.close();
+        int sum  = n*(n+1)/2; // formula for sum of first N natural numbers
+        System.out.println("Sum of first N natural numbers is : " + sum);
+        System.out.println("Sum of first N natural numbers is : " + n*(n+1)/2); // more efficient way
+
     }
 }
